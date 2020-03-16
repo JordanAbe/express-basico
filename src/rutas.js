e3e586d6fd18960be0bee6b873ce5b134c5e860b
Jordan Abe <j.abe@costamar.com>
2020-03-16 18:26:31 -0500

    se agrega middleware general y especìfico

--- src/rutas.js
+++ src/rutas.js
@@ -5,12 +5,47 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/test', routeHelper(async (req, res) => {
+//middleware aplica para todos
+app.use( (req, res, next) => {
+    if(req.ip === '140.23.435.2'){
+        next(new Error('error !!!'))
+    } else {
+        next();
+    }
+})
+
+function getUser(){
+    return {
+        id: '123',
+        name: 'jordan',
+    }
+}
+
+//aplicación específica
+const middleware = async (req, res, next) => {
+    const user = await getUser();
+    req.locals = {
+        user,
+    }
+    next();
+};
+
+app.get('/test', middleware, routeHelper(async (req, res) => {
     // throw new Error('error crítico');
-    await addUser();
-    await sleep(3000);
+    // await addUser();
+    // await sleep(3000);
+    user = req.locals.user;
     res.json({
-        status: 'ok'
+        status: 'ok',
+        user,
+    })
+}));
+
+app.get('/test-2', routeHelper(async (req, res) => {
+    user = req.locals.user;
+    res.json({
+        status: 'ok',
+        user,
     })
 }));
 
